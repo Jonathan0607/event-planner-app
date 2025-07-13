06ff763b92daa969108f79d84d63c7d3dfc9f08d
Jonathan0607 <jonathjve.43@gmail.com>
2025-07-13 13:02:13 -0500

    added logo

---
+++ app/(auth)/login/[[...login]]/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+function LoginPage() {
+    return (
+        <main className ="mt-15">
+            <Image
+                src = '/assets/logo.svg'
+                alt = 'Logo'
+            />
+        </main>
+    );
+}
+
+export default LoginPage;
