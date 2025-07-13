008132849d4abf558caaffbef1b252f38e99a26d
Jonathan0607 <jonathjve.43@gmail.com>
2025-07-13 15:23:30 -0500

    login layout

--- app/(auth)/login/[[...login]]/page.tsx
+++ app/(auth)/login/[[...login]]/page.tsx
@@ -5,13 +5,15 @@ import Image from "next/image";
 function LoginPage() {
     return (
         <main className ="flex flex-col items-center p-5 gap-10 animate-fade-in">
+            <div></div>
             <Image
                 src = '/logo.svg'
                 width = {100}
                 height = {100}
                 alt = 'Logo'
+                className=""
             />
-            <div className = "mt-3">
+            <div className = "">
                 <SignIn/>
             </div>
         </main>
