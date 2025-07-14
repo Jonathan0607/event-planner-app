7e525c568bc3bd8503a0f7845ca6c099a36b5ec8
Jonathan0607 <jonathjve.43@gmail.com>
2025-07-14 11:38:17 -0500

    landing page done

--- components/landingPage.tsx
+++ components/landingPage.tsx
@@ -1,26 +1,46 @@
 'use client'
 
-import Image from "next/image";
-import { SignOutButton } from '@clerk/nextjs'
+import { SignIn } from "@clerk/nextjs"
+import { neobrutalism } from "@clerk/themes"
+import Image from "next/image"
 
-function LandingPage() {
-    return ( 
-    <main className="flex items-center p-10 gap-24 animate-fade-in max-md:flex-col">
-        <section className="flex items-center flex-col">
+export default function LandingPage() {
+    return (
+        <main className="flex items-center p-10 gap-24 animate-fade-in max-md:flex-col">
+        <section className="flex flex-col items-center">
             <Image
-            src="/logo.svg"
-            height= {100}
-            width = {100}
-            alt= "Logo"
+            src='/logo.svg'
+            width={300}
+            height={300}
+            alt="Logo"
             />
-            <h1 className = "text-2xl font-bold lg:text-3xl">Your Time, Perfectly Planned</h1>
-            <p>Random Description for the product</p>
+
+            <h1 className="text-2xl font-black lg:text-3xl">
+            Your time, perfectly planned
+            </h1>
+
+            <p className="font-extralight">
+            Join millions of professionals who easily book meetings with the #1 scheduling tool
+            </p>
+
+            <Image
+            src='/person.svg'
+            width={500}
+            height={500}
+            alt="Logo"
+            />
         </section>
-        
 
+        <div className="flex-1 flex items-center justify-center w-full">
+            <SignIn
+            routing="hash" 
+            appearance={{
+                baseTheme: neobrutalism, 
+            }}
+            />
+        </div>
 
-    </main> 
-    );
+        </main>
+
+    )
 }
-
-export default LandingPage;
